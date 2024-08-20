bb83fe3f5e099c01d1c3db72e024d5b3550d3bd1
Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>
2024-08-20 13:46:47 +0200

    third commit

--- searching/sentinel_linear_search.py
+++ searching/sentinel_linear_search.py
@@ -10,7 +10,7 @@
 # Note: This algorithm is a modified version of the linear search algorithm.
 # The sentinel linear search algorithm uses a sentinel value to reduce the number of comparisons.
 # The sentinel value is placed at the end of the list, and the loop is terminated when the sentinel value is found.
-def linear_search(A, n, x):
+def sentinel_linear_search(A, n, x):
     """
     Inputs:
         A: list of integers
@@ -19,7 +19,12 @@ def linear_search(A, n, x):
 
     Output: index of x in A, -1 if x is not in A
     """
-    for i in range(n):
-        if A[i] == x:
-            return i
+    last = A[n - 1]
+    A[n - 1] = x
+    i = 0
+    while A[i] != x:
+        i += 1
+    A[n - 1] = last
+    if i < n - 1 or A[n - 1] == x:
+        return i
     return -1
